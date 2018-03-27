6534f3c93263bb0a3871a7d2b77d670f518facde
Michael DeBonis <mike@devised.io>
2018-03-27 06:36:43 -0500

    Renaming files

--- postgres/mm_set_user_defaults.sql
+++ postgres/mm_set_user_defaults.sql
@@ -1,9 +1,17 @@
-CREATE OR REPLACE FUNCTION insert_theme() RETURNS TRIGGER AS
+CREATE OR REPLACE FUNCTION mm_set_newuser_defaults() RETURNS TRIGGER AS
 $BODY$
 BEGIN
-  IF (SELECT COUNT(*) FROM Preferences WHERE UserId=NEW.Id AND 'Category'='theme') = 0 THEN
+
+  IF (SELECT COUNT(*) FROM Preferences
+  WHERE UserId=NEW.Id AND 'Category'='theme') = 0 THEN
   INSERT INTO Preferences (UserId, Category, Name, Value)
   VALUES (NEW.Id, 'theme', '', '{"awayIndicator":"#23b6c9","buttonBg":"#77c373","buttonColor":"#ffffff","centerChannelBg":"#fdfdfd","centerChannelColor":"#4b4b4b","codeTheme":"solarized-dark","errorTextColor":"#ff6461","linkColor":"#77c373","mentionBj":"#b74a4a","mentionColor":"#ffffff","mentionHighlightBg":"#e8ffe7","mentionHighlightLink":"#77c373","newMessageSeparator":"#77c373","onlineIndicator":"#77c373","sidebarBg":"#093f4b","sidebarHeaderBg":"#08353f","sidebarHeaderTextColor":"#ffffff","sidebarText":"#ffffff","sidebarTextActiveBorder":"#77c373","sidebarTextActiveColor":"#ffffff","sidebarTextHoverBg":"#156573","sidebarUnreadText":"#ffffff"}');
+  END IF;
+
+  IF (SELECT COUNT(*) FROM Preferences
+  WHERE UserId=NEW.Id AND 'Category'='advanced_settings' AND 'Name'='join_leave') = 0 THEN
+  INSERT INTO Preferences (UserId, Category, Name, Value)
+  VALUES (NEW.Id, 'advanced_settings', 'join_leave', 'false');
   END IF;
 
   RETURN NEW;
@@ -11,8 +19,8 @@ END;
 $BODY$
 LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS default_newuser_theme ON Users;
-CREATE TRIGGER default_newuser_theme
+DROP TRIGGER IF EXISTS mm_on_newuser ON Users;
+CREATE TRIGGER mm_on_newuser
 AFTER INSERT ON Users
 FOR EACH ROW
-EXECUTE PROCEDURE insert_theme();
+EXECUTE PROCEDURE mm_set_newuser_defaults();
